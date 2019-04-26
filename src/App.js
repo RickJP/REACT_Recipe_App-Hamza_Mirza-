fcb2c30d6a031b0c8305da05712bacce242f2a7a
rickjp <3741326+RickJP@users.noreply.github.com>
2019-04-26 21:22:10 +0900

    Successfully got recipe data from food2fork API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from "./components/Form";
 
+const API_KEY = "3d02782f74a7fbc128e61977c8614a41";
+
+
+
 class App extends Component {
-  getRecipe = (e) => {
+
+
+  state = {
+    recipes: []
+  }
+
+
+  getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    console.log(recipeName);
+    const api_call = await fetch
+    (`https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=2`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
   }
+
   render() {
     return (
       <div className="App">
@@ -15,9 +32,15 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        {this.state.recipes.map((recipe) => {
+          return <p key={recipe.recipe_id}>{ recipe.title }</p>
+        }) }
       </div>
     );
   }
 }
 
 export default App;
+
+// food2fork API KEY
+// 3d02782f74a7fbc128e61977c8614a41

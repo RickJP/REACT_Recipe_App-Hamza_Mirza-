d6b1e3f95b2dc4fd642ceb3117ea8b70bbfa2560
rickjp <3741326+RickJP@users.noreply.github.com>
2019-04-27 00:07:54 +0900

    Now displays recipes in cards more beautifully

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "3d02782f74a7fbc128e61977c8614a41";
 
-
-
 class App extends Component {
-
-
   state = {
     recipes: []
-  }
+  };
 
-
-  getRecipe = async (e) => {
+  getRecipe = async e => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch
-    (`https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=2`);
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=2`
+    );
 
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
-  }
+  };
 
   render() {
     return (
@@ -31,16 +28,11 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form getRecipe={this.getRecipe}/>
-        {this.state.recipes.map((recipe) => {
-          return <p key={recipe.recipe_id}>{ recipe.title }</p>
-        }) }
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
 }
 
 export default App;
-
-// food2fork API KEY
-// 3d02782f74a7fbc128e61977c8614a41
